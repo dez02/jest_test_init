a554160a8ba1e2d9cc462dbe375e6e53ac560c7f
Désirée <deutodesiree@gmail.com>
2020-04-17 23:54:09 +0200

    added substract, multiplication and division tests

--- calculator.spec.js
+++ calculator.spec.js
@@ -1,6 +1,18 @@
 const calculator = require('./calcutor');
 
 test('adds 1 + 2 to equal 3', () => {
-  expect(toto.add(1, 2)).toBe(3);
+  expect(calculator.add(1, 2)).toBe(3);
+});
+
+it('should work', () => {
+  expect(calculator.sub(8, 4)).toBe(4);
+});
+
+it('should work', () => {
+  expect(calculator.mult(4,9)).toBe(36);
+});
+
+it('Should work', ()  => {
+  expect(calculator.div(9, 3)).toBe(3);
 });
 
--- calcutor.js
+++ calcutor.js
@@ -1,4 +1,14 @@
 // SUM
 exports.add = (a, b) => a + b;
 
+//SUB
+exports.sub = (a, b) => a - b;
+
+// mult
+exports.mult = (a, b) => a * b;
+
+//div
+
+exports.div = (a, b) => a / b;
+
 

3bb22a90751e741586feb27b91ec91949918f3e9
Désirée <deutodesiree@gmail.com>
2020-04-18 00:19:27 +0200

    handled exceptions

--- calcutor.js
+++ calcutor.js
@@ -1,14 +1,66 @@
 // SUM
-exports.add = (a, b) => a + b;
+exports.add = (a, b) => {
+	if (arguments.length !== 2) {
+		console.log("expected two arguments");
+	}
+
+	if(typeof b !== Number) {
+		console.log("It's not an number!");
+	}
+
+	if(typeof a !== Number) {
+		console.log("It's not an number!");
+	}
+	return a + b;
+};
 
 //SUB
-exports.sub = (a, b) => a - b;
+exports.sub = (a, b) => {
+	if (arguments.length !== 2) {
+		console.log("Too much arguments");
+	}
+
+	if(typeof b !== Number) {
+		console.log("It's not an number!");
+	}
+
+	if(typeof a !== Number) {
+		console.log("It's not an number!");
+	}
+	return a - b;
+} 
 
 // mult
-exports.mult = (a, b) => a * b;
+exports.mult = (a, b) => {
+	if (arguments.length !== 2) {
+		console.log("Too much arguments");
+	}
+
+	if(typeof b !== Number) {
+		console.log("It's not an number!");
+	}
+
+	if(typeof a !== Number) {
+		console.log("It's not an number!");
+	}
+	return a * b;
+} 
 
 //div
 
-exports.div = (a, b) => a / b;
+exports.div = (a, b) => {
+	if (arguments.length !== 2) {
+		console.log("Too much arguments");
+	}
+
+	if(typeof b !== Number) {
+		console.log("It's not an number!");
+	}
+
+	if(typeof a !== Number) {
+		console.log("It's not an number!");
+	}
+	return a / b;
+}
 
 
